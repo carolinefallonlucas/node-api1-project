36095d7157a7804e3034f9704fdeda40bc381f8d
Caroline <carolinefallonlucas@gmail.com>
2021-01-14 02:57:51 -0500

    cors added and database.js created

---
+++ database.js
@@ -0,0 +1,8 @@
+
+const Users = {
+  id: "a_unique_id", // hint: use the shortid npm package to generate it
+  name: "Jane Doe", // String, required
+  bio: "Not Tarzan's Wife, another Jane",  // String, required
+}
+
+export default Users
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+import {Users} from "./database" 
+
 const express = require('express');
+const cors = require("cors");
+const shortid = require('shortid');
+
 const server = express();
+server.use(express.json());
+server.use(cors());
 
 
 server.get('/', (req, res) =>
 {
     res.send('Heck yeah, heck yeah');
 
-})
+}) // READ data
+
+server.post('/', (req, res) =>
+{
+    res.send('Heck yeah, heck yeah');
+
+}) // CREATE data
+
 
 server.listen(5000, ( ) =>{
     console.log('Heck yeah, we are up and running on local host 5000')
 })
+
